a4452fa8e68067c1a955cb6f9e546c5f1a9d43ba
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-21 20:38:16 -0500

    dev

---
+++ studio/Rocket.ts
@@ -0,0 +1,31 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[]=[]; 
+    astronauts: Astronaut[]=[];
+    
+    constructor(name: string, totalCapacityKg : number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    } 
+    sumMass(items: Payload[]): number {
+        let totMass: number = 0;
+        for(let i=0; i < items.length; i++) {
+            totMass += items[i].massKg;
+        }
+        return totMass;
+     }   
+     currentMassKg(): number {
+        let totCargoMass = this.sumMass(this.cargoItems);
+        let totAstronautMass = this.sumMass(this.astronauts);
+        let totCurrMass= totCargoMass + totAstronautMass;
+        return totCurrMass; 
+
+     }
+     //Uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems  
+} 
